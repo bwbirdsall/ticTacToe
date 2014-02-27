51894e68f1c5ff45f9030723906a9b7849a2c734
Ben Birdsall & Tara Nelson <student@epicodus.com>
2014-02-27 11:24:15 -0800

    adds all manner of tic-tac-toe functionality; no website or jQuery worth mentioning.

---
+++ js/scripts.js
@@ -0,0 +1,121 @@
+var Space = {
+  all: [],
+  create: function(x, y) {
+    var newSpace = Object.create(Space);
+    newSpace.x = x;
+    newSpace.y = y;
+    newSpace.value = "";
+    this.all.push(newSpace);
+    return newSpace;
+  },
+  find: function(x,y) {
+    var match = false;
+    Space.all.forEach(function(space) {
+      if(space.x === x) {
+        if(space.y === y) {
+          match = space;
+        }
+      }
+    });
+    return match;
+  },
+  markedBy: function(player) {
+    if(this.value === "") {
+      this.value = player;
+      return this.value;
+    } else {
+      return "Nope";
+    }
+  },
+  returnCoordinates: function() {
+    return [this.x, this.y]; 
+  },
+  returnValue: function() {
+    return this.value;
+  }
+}
+
+var Player = {
+  create: function(faction) {
+    var newPlayer = Object.create(Player);
+    newPlayer.faction = faction;
+    return newPlayer;
+  }
+}
+
+var Board = {
+  create: function() {
+    var newBoard = Object.create(Board);
+    newBoard.boardSpaces = [];
+    newBoard.fillBoard();
+    return newBoard;
+  },
+  fillBoard: function() {
+    for(var x = 1; x < 4; x++) {
+      for(var y = 1; y < 4; y++) {
+        this.boardSpaces.push(Space.create(x,y));
+      }
+    }
+  },
+  whoIsWinner: function() {
+    var winner = false;
+    for(var x = 1; x < 4; x++) {
+      var space1 = Space.find(x,1);
+      var space2 = Space.find(x,2);
+      var space3 = Space.find(x,3);
+      if(space1.value === space2.value && space1.value === space3.value && space1.value != "") {
+        winner = space1.value;
+      }
+    }
+    for(var y = 1; y < 4; y++) {
+      var space1 = Space.find(1,y);
+      var space2 = Space.find(2,y);
+      var space3 = Space.find(3,y);
+      if(space1.value === space2.value && space1.value === space3.value && space1.value != "") {
+        winner = space1.value;
+      }
+    }
+    if(Space.find(1,1).value === Space.find(2,2).value && Space.find(1,1).value === Space.find(3,3).value) {
+      winner = Space.find(1,1).value;
+    } else if(Space.find(1,3).value === Space.find(2,2).value && Space.find(1,3).value === Space.find(3,1).value) {
+      winner = Space.find(1,3).value;
+    }
+    return winner;
+  }
+}
+
+var Game = {
+  create: function() {
+    var newGame = Object.create(Game);
+    newGame.board = Board.create();
+    newGame.playerX = Player.create("X");
+    newGame.playerY = Player.create("Y");
+    newGame.whoseTurn = newGame.whoStarts();
+    return newGame;
+  },
+  gameOver: function() {
+    if(this.board.whoIsWinner() !== false) {
+      return this.board.whoIsWinner();
+    } else if (this.board.boardSpaces.every(function(space){
+        return space.faction !== "";
+      })) {
+      return "NOBODY WINS";
+    } else {
+      return false;
+    }
+  },
+  switchTurn: function() {
+    if(this.whoseTurn === this.playerX.faction) {
+      this.whoseTurn = this.playerY.faction;
+    } else {
+      this.whoseTurn = this.playerX.faction;
+    }
+  },
+  whoStarts: function() {
+    if(Math.round(Math.random()) === 1) {
+      return this.playerY.faction;
+    } else {
+      return this.playerX.faction;
+    }
+  }
+}
---
+++ spec/specs.js
@@ -0,0 +1,120 @@
+beforeEach(function() {
+  Space.all = [];
+});
+
+describe('Space', function() {
+  describe('returnCoordinates', function() {
+    it('returns the coordinates x,y', function() {
+      var testSpace = Space.create(1,2);
+      testSpace.returnCoordinates().should.eql([1,2]);
+    });
+  });
+  describe('returnValue', function() {
+    it('returns the current value at the Space: X, O or Empty', function() {
+      var testSpace = Space.create(1,2);
+      testSpace.value = 'X';
+      testSpace.returnValue().should.equal('X');
+    });
+  });
+  describe('markedBy', function() {
+    it('sets the value of the Space depending on the player passed in', function() {
+      var testSpace = Space.create(1,2);
+      testSpace.markedBy('X').should.equal('X');
+    });
+    it('returns an error if the Space has already been marked', function() {
+      var testSpace = Space.create(1,2);
+      testSpace.markedBy('X');
+      testSpace.markedBy('0').should.equal('Nope');
+    });
+  });
+  describe('find', function() {
+    it('returns the Space belonging to provided coordinates', function() {
+      var testSpace = Space.create(1,2);
+      var testSpaceTwo = Space.create(1,3);
+      Space.find(1,3).should.equal(testSpaceTwo);
+    });
+  });
+});
+
+describe('Player', function() {
+  it('returns whether the player belongs to the X or O faction', function() {
+    var newPlayer = Player.create('X');
+    newPlayer.faction.should.equal('X');
+  });
+});
+
+describe('Board', function(){
+  describe('create', function() {
+    it('creates a board with 9 spaces', function() {
+      var newBoard = Board.create();
+      newBoard.boardSpaces.length = 9;
+    });
+  });
+  describe('whoIsWinner', function() {
+    it('returns the first player to mark three spaces in a row', function() {
+      var newBoard = Board.create();
+      Space.find(1,2).markedBy('X');
+      Space.find(1,1).markedBy('X');
+      Space.find(1,3).markedBy('X');
+      newBoard.whoIsWinner().should.equal('X');
+    });
+    it('returns the winning faction for diagonal victory states', function() {
+      var newBoard = Board.create();
+      Space.find(1,1).markedBy('X');
+      Space.find(2,2).markedBy('X');
+      Space.find(3,3).markedBy('X');
+      newBoard.whoIsWinner().should.equal('X');
+    });
+  });
+});
+
+describe('Game', function() {
+  describe('create', function() {
+    it('creates a game containing a proper board', function() {
+      var newGame = Game.create();
+      newGame.board.boardSpaces.length.should.equal(9);
+    });
+    it('creates two warring factions', function() {
+      var newGame = Game.create();
+      newGame.playerX.faction.should.equal('X');
+      newGame.playerY.faction.should.equal('Y');
+    });
+  });
+  describe('switchTurn', function() {
+    it("switches which player's turn it is", function() {
+      var newGame = Game.create();
+      newGame.whoseTurn = 'X';
+      newGame.switchTurn();
+      newGame.whoseTurn.should.equal('Y');
+    });
+  });
+  describe('gameOver', function() {
+    it('halts the game once a winner is determined', function() {
+      var newGame = Game.create();
+      Space.find(1,2).markedBy('X');
+      Space.find(1,1).markedBy('X');
+      Space.find(1,3).markedBy('X');
+      newGame.gameOver().should.equal('X');
+    });
+    it('halts the game if there are no more moves left', function() {
+      var newGame = Game.create();
+      Space.find(1,2).markedBy('A');
+      Space.find(1,1).markedBy('B');
+      Space.find(1,3).markedBy('C');
+      Space.find(2,2).markedBy('D');
+      Space.find(2,1).markedBy('E');
+      Space.find(2,3).markedBy('F');
+      Space.find(3,2).markedBy('G');
+      Space.find(3,1).markedBy('H');
+      Space.find(3,3).markedBy('I');
+      newGame.gameOver().should.equal('NOBODY WINS');
+    });
+  });
+  describe('whoStarts', function() {
+    it('returns a random game starting faction', function() {
+      var newGame = Game.create();
+      newGame.whoseTurn.should.equal("X");
+      newGame.whoseTurn.should.equal("Y");
+    });
+  });
+});
